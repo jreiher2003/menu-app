c97b573a44ce7989132bdbb74b8f2b1acf1533dc
Jeff Reiher <jeffreiher@gmail.com>
2015-11-17 13:16:38 -0500

    add SQLALCHEMY_TRACK_MODIFICATIONS = True to config

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ SECRET_KEY = 'you-will-never-guess'
 
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'menu.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
