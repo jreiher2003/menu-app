25d99e26494e5a3bd7ec840065b9e1a57ef54375
Jeff Reiher <jeffreiher@gmail.com>
2015-11-17 13:06:51 -0500

    initalize db

--- menu/models.py
+++ menu/models.py
@@ -1,18 +1,22 @@
-import sys
+# from sqlalchemy import Column, ForeignKey, Integer, String
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import relationship
+# from sqlalchemy import create_engine
+
+
+# Base = declarative_base()
 from menu import db
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
-
-
-Base = declarative_base()
 #### class code #######
-class Restaurant(Base):
-	__tablename__ = 'restaurant' # names table
-	name = Column(String(80), nullable=False)
-	id = Column(Integer, primary_key=True)
+class Place(db.Model):
+    __tablename__ = 'place' # names table
+    name = db.Column(db.String(80), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
 
+    def __repr__(self):
+        return '<Place %r>' % (self.name)
+
+
+        
 ########## insert at end of file ########
-engine = create_engine('sqlite:///menu.db')
-Base.metadata.create_all(engine)
+# engine = create_engine('sqlite:///menu.db')
+# Base.metadata.create_all(engine)
--- menu/views.py
+++ menu/views.py
@@ -9,13 +9,21 @@ from menu import app
 # engine = create_engine('sqlite:///menu.db')
 # Base.metadata.bind = engine 
 
+
 @app.route('/')
 @app.route('/hello')
 def index():
-    # return render_template('base.html')
-    return 'Hello Jeffrey'
-    # place = session.query(Place).all()
-    # for p in place:
-    # 	output += p.name
-    # 	output += '<br>'
-    # return output
+    user = {'nickname': 'Jeffaroni'}  
+    posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+    return render_template('base.html', title="Jeff's boom boom room",
+    									user=user,
+    									posts=posts)
